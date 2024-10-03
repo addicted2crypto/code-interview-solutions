920cac85d8c84f5add4f5295e0fe9a771106690e
William(addicted2crypto) <addicted2krypto@gmail.com>
2024-10-02 17:28:23 -0700

    BS prob

--- JS/binary.js
+++ JS/binary.js
@@ -2,6 +2,8 @@
 // Given a sorted array of integers and an integer called target, find the element that 
 // equals the target and return its index. If the element is not found return -1.
 
+//BS Problem
+
 
 
 
