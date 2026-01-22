0f06f99f8a7ae6408684be54099d70da99c677c1
William(addicted2crypto) <addicted2krypto@gmail.com>
2026-01-22 06:32:44 -0800

    asc && desc

--- employeeDirApi/hackerrank/findTheFactor.js
+++ employeeDirApi/hackerrank/findTheFactor.js
@@ -19,7 +19,7 @@
 // 1 <= = <= 10^9 
 
 //BigInt didn't work in hackerrand so had to get a brute force on the board, will optimize to run more efficiently later 
-//I will add a const small and const large array to sort as I go to avoid sorting at the end
+//I will add a const small and const large array to sort as I go to avoid sorting at the end asc and descending
 
 function findTheFactor(n, p) {
     const factors = [];
